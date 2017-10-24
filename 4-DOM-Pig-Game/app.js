8aed00cc5769545aeef8682e42a2f28fd6e3e0bb
daniel <daniel10x@hotmail.com>
2017-10-23 22:10:28 -0200

    Continuando as aulas

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -11,22 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, dice;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-//Le o conteudo do elemento
-var x = document.querySelector('#score-0').textContent;
-console.log(x);
-
-//Tratar o css do elemento. No caso, o none deixa em "branco"
-document.querySelector('.dice').style.display = 'none';
-
-//Busca o elemento pelo Id, e mais rapido que o querySelector
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
 	// Crio um numero aleatorio entre 1 e 6.
@@ -35,10 +20,70 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
 	var diceDOM = document.querySelector('.dice');
 	diceDOM.style.display = 'block';
 	diceDOM.src = 'dice-' + dice + '.png';
+
+	if(dice !== 1) {
+		// Somo ao score
+		roundScore += dice;
+		//Insere o valor apenas como um texto puro
+		document.querySelector('#current-' + activePlayer).textContent = dice;
+	} else {
+		// Operador ternario
+		nextPlayer();
+	}
 });
 
+document.querySelector('btn-hold').addEventListener('click', function() {
+	//Soma o placar atual ao GLOBAL
+  scores[activePlayer] += roundScore;
+	//Atualiza a interface
+  document.querySelector('#score' + activePlayer).textContent = scores[activePlayer];
+	//Verifica se e vencedor
+  if(scores[activePlayer] >= 100) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+    document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else {
+		nextPlayer();
+	}
 
-//Insere o valor apenas como um texto puro
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+});
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+
+	//document.querySelector('.player-0-panel').classList.remove('active');
+	//document.querySelector('.player-1-panel').classList.add('active');
+	//O toggle faz o mesmo.
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+	document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('btn-new').addEventListener('click', init);
+
+function init() {
+	scores = [0, 0];
+	roundScore = 0;
+	activePlayer = 0;
+	//Tratar o css do elemento. No caso, o none deixa em "branco"
+	document.querySelector('.dice').style.display = 'none';
+	//Busca o elemento pelo Id, e mais rapido que o querySelector
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+}
 //Insere o fragmento como um HTML
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
